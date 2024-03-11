24f9732691f36075f29f7c1fcbb57244ebd672df
FilArt <artfil@protonmail.com>
2024-03-11 12:00:55 +0100

    allow using offline mode

--- utils/api.ts
+++ utils/api.ts
@@ -1,4 +1,4 @@
-import { API_URL_SERVER } from '@env';
+import { API_URL_SERVER, MODE } from '@env';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
@@ -7,6 +7,22 @@ import { Platform } from "react-native";
 const api = axios.create({
   baseURL: API_URL_SERVER,
 });
+
+if (MODE === 'offline') {  
+  api.post = (args) => {
+    console.log(args)
+    return new Promise((resolve) => resolve({ data: { token: "testtoken" } }));
+  };
+
+  api.get = (url) => {
+    if (url === '/agent_agenda/agenda/' ) {
+      return new Promise((resolve) => resolve({ data: require('/agenda_events.json') }));
+    }
+    console.log(url)
+    return new Promise((resolve) => resolve({ data: { } }));
+  }
+}
+
 const TOKEN_KEY = "token";
 
 
