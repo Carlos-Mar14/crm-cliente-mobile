43fd392e4bec171578f2af4c9ea6012ea476ddac
FilArt <artfil@protonmail.com>
2024-04-10 23:01:17 +0200

    fix safe area

--- App.tsx
+++ App.tsx
@@ -1,9 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import 'react-native-gesture-handler';
-import { PaperProvider } from 'react-native-paper';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AuthProvider from './components/AuthContext';
 import { DrawerNavigation } from './navigations/DrawerNavigation';
 
@@ -11,18 +9,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <AuthProvider>
-        <PaperProvider>
-          <SafeAreaView style={styles.container}>
-            <DrawerNavigation />
-          </SafeAreaView>
-        </PaperProvider>
+        <SafeAreaProvider >
+          <DrawerNavigation />
+        </SafeAreaProvider>
       </AuthProvider>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  }
-})
