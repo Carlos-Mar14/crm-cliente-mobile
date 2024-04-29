3bd55192d0a026bd3091756764bb7aae27982a84
FilArt <artfil@protonmail.com>
2024-04-29 22:48:24 +0200

    fix customer hook

--- components/customer/hooks/customer.tsx
+++ components/customer/hooks/customer.tsx
@@ -1,41 +1,41 @@
-import { useEffect, useState } from "react";
-import { api } from "../../../utils/api";
+import { useEffect, useState } from 'react'
+import { api } from '../../../utils/api'
 
 interface ICustomer {
-  id?: number;
-  name?: string;
-  agent?: string;
-  operator?: string;
-  last_modified?: string;
-  created_at?: string;
-  status?: string;
-  persona_contacto?: string;
-  email?: string;
-  cargo?: string;
-  schedule?: string;
-  cnae?: string; // CNAE
-  persona_contacto_email?: string;
-  client_type?: string;
-  document_type?: string;
-  dni?: string;
-  factura_en_papel?: string;
+  id?: number
+  name?: string
+  agent?: string
+  operator?: string
+  last_modified?: string
+  created_at?: string
+  status?: string
+  persona_contacto?: string
+  email?: string
+  cargo?: string
+  schedule?: string
+  cnae?: string // CNAE
+  persona_contacto_email?: string
+  client_type?: string
+  document_type?: string
+  dni?: string
+  factura_en_papel?: string
 }
 
-export const useCustomer = (id) => {
-  const [customer, setCustomer] = useState<ICustomer | null>(null);
+export const useCustomer = (id: number) => {
+  const [customer, setCustomer] = useState<ICustomer | null>(null)
 
   useEffect(() => {
-    getCustomerData();
-  });
-
-  async function getCustomerData() {
-    try {
-      const response = await api.get<ICustomer>(`/cards/${id}/`);
-      setCustomer(response.data);
-    } catch (error) {
-      setCustomer(null);
+    async function getCustomerData() {
+      try {
+        const response = await api.get<ICustomer>(`/cards/${id}/`)
+        setCustomer(response.data)
+      } catch (e) {
+        console.error(e)
+        setCustomer(null)
+      }
     }
-  }
+    getCustomerData()
+  }, [id])
 
-  return { customer };
-};
+  return { customer }
+}
